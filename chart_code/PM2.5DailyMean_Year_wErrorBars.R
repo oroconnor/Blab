ff7edfe8a3784451668c5972707a735a952e52a5
Owen O'Connor <46877624+oroconnor@users.noreply.github.com>
2021-01-21 15:55:15 -0500

    Update PM2.5DailyMean_Year_wErrorBars.R
    With updated Error bars

--- chart_code/PM2.5DailyMean_Year_wErrorBars.R
+++ chart_code/PM2.5DailyMean_Year_wErrorBars.R
@@ -4,22 +4,19 @@ library(ggplot2)
 library(feather)
 
 #read in the tibble prepared by airmasterk.R script
-airmaster <- read_feather("airmasterk.feather")
+airmasterk <- read_feather("airmasterk.feather")
 
 #creates new hour and date columns, and a column for the log10 of the pm2.5 calculation
 airmasterk <- airmasterk %>%
   mutate(hour = hour(Time), date = as_date(Time),
-         lnpm2.5 = log10(pm2.5)
   )
 
 #groups be the date and does calculations for the error bars
 airmasterk <- airmasterk %>%
   group_by(date) %>% #group by the date
   summarise(daily_mean = mean(pm2.5), #daily pm2.5 mean
-            daily_lnmean = mean(lnpm2.5), #daily natural log pm2.5
-            sdlnpm2.5 = sd(lnpm2.5), #standard deviation of daily natural log pm2.5
-            selnpm2.5 = (sdlnpm2.5)/sqrt(1440), #standard error of daily natural log pm2.5
-            sepm2.5 = 10 ^ selnpm2.5
+            sdpm2.5 = sd(pm2.5), #standard deviation of daily pm2.5
+            sepm2.5 = (sdpm2.5)/sqrt(1440), #standard error of daily pm2.5
   ) %>%
   na.omit()
 
@@ -32,16 +29,21 @@ airmasterk <- airmasterk %>%
 #generates the year point plot with error bars
 ggplot(data = airmasterk,
        aes(x = YMD,y = daily_mean)) +
-  geom_point(aes(y = daily_mean), color = "red", size= 1) +
+  geom_point(aes(y = daily_mean), color = "red", size= 1.4) +
   geom_errorbar(aes(ymin = daily_mean - sepm2.5, ymax = daily_mean + sepm2.5), size = .2, width = 1,alpha = .3) +
+  geom_smooth(se = FALSE) +
+  geom_hline(yintercept=35, color="orange", lty=2) +
+  annotate("text", x = as.Date("2020-11-15", "%Y-%m-%d"),  y = 37, label = "EPA 24-hr Avg. Threshold") +
   #geom_line(aes(y = daily_mean), color = "red") +
-  scale_x_date(date_breaks = "months" , date_labels = "%b") +
+  scale_x_date(limits = as.Date(c("2020-01-16","2021-01-16")), date_breaks = "months", date_labels = "%b") +
   labs(
-    y = "PM2.5",
+    y = "PM2.5 - mg/cubic meter - 24 hr. Avg.",
     x = "Date",
     title = paste(
       "2020 Particulate Matter - Kingston"
     ),
     subtitle = paste(
       "PM2.5 Daily Averages"
-    ))
+    )) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.subtitle = element_text(hjust = 0.5))
